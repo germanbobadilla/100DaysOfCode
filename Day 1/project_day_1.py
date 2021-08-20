8b8cc179e7ef3db125653137b603b7de167b8ff3
German Bobadilla <germanbobadilla@gmail.com>
2021-08-20 08:11:22 -0400

    couple of changes

--- Day 1/project_day_1.py
+++ Day 1/project_day_1.py
@@ -8,22 +8,22 @@ she_or_he = she_or_he.lower() # Puts in lower case
 she_or_he = she_or_he.strip() # Trims spaces
 
 if she_or_he == "he":    # Depending on the input, if it's a boy
-    print("Bot: Great! Let's find a name for you baby boy")    
+    print("Bot: Great! Let's find a name for your baby boy")    
     spose_name1 = input("What's your spouse name? ") # Saves the value in var
     spose_name2 = input("What's your name? ")
     first_three_char1 = spose_name1[0:3] # Takes the first three letters of the name
     first_three_char2 = spose_name2[0:3]
     first_three_char1 = first_three_char1.lower() # Changes to lowercase
     first_three_char2 = first_three_char2.lower()
-    print("Wonderful! The name is " + first_three_char1.capitalize() + first_three_char2 + ".\n Do you think he'll like it?")
+    print("Wonderful! The name is " + first_three_char1.capitalize() + first_three_char2 + ".\nDo you think he'll like it?")
 elif she_or_he == "she": # If it's a baby girl
     she = "she"
-    print("Bot: Great! Let's find a name for you baby girl")    
+    print("Bot: Great! Let's find a name for your baby girl")    
     spose_name1 = input("What's your spouse name? ") # Saves the value in var
     spose_name2 = input("What's your name? ")
     first_three_char1 = spose_name1[0:3] # Takes the first three letters of the name
     first_three_char2 = spose_name2[0:3]
     first_three_char1 = first_three_char1.lower() # Changes to lowercase
     first_three_char2 = first_three_char2.lower()
-    print("Wonderful! The name is " + first_three_char1.capitalize() + first_three_char2 + ".\n Do you think she'll like it?")
+    print("Wonderful! The name is " + first_three_char1.capitalize() + first_three_char2 + ".\nDo you think she'll like it?")
     # prints the first three letters of each parent with the first letter in uppercase
