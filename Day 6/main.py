3d3469b5db5fea618283fbb27870a2bc27fe7ade
German Bobadilla <germanbobadilla@gmail.com>
2021-08-26 19:57:38 -0400

    Day 6

--- Day 6/main.py
+++ Day 6/main.py
@@ -6,6 +6,7 @@ older_brothers = ['Momon', 'James']
 nested_list = [brothers, older_brothers]
 
 print(nested_list[0])
+
 # Exercise 3 - Treasure map
 
 row1 = ['🤍', '🤍', '🤍']
@@ -25,3 +26,14 @@ selected_row[horizontal - 1] = heart
 
 
 print(f"{row1}\n{row2}\n{row3}\n")
+
+# Exercise 4
+
+import math
+
+def paint_calc(height,width,cover):
+
+    num_of_cans = math.ceil(height * width / cover)
+    print(f"You'll need {num_of_cans} cans of paint.")
+    
+paint_calc(13, 90, 35)
