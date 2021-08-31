5b116aa179fb7ecbeda50c12fbef5dd07325c4d4
German Bobadilla <germanbobadilla@gmail.com>
2021-08-31 06:43:04 -0400

    Day 6 and 7

---
+++ Day 7/main.py
@@ -0,0 +1,17 @@
+import numpy as np
+import pandas as pd
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+
+plt.close("all")
+ts = pd.Series(np.random.rand(1000), index=pd.date_range("1/1/2000", periods=1000))
+ts = ts.cumsum()
+
+df = pd.DataFrame(np.random.randn(1000, 4), index=ts.index, columns=["A", "B", "C", "D"])
+df = df.cumsum()
+plt.figure()
+df.plot()
+plt.legend(loc='best')
+
+plt.show()
